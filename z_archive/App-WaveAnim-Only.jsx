1897b97ed4227622738aa65c3751b3ef2305b5bf
Aaron Berkson <hello@aaronberkson.com>
2025-07-29 23:53:31 -0400

    Remove z_archive and slim down repo

--- z_archive/App-WaveAnim-Only.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import WaveAnimation from "./three/WaveAnimation"; // Import Three.js animation
-
-const App = () => {
-  return <WaveAnimation />;
-};
-
-export default App;
